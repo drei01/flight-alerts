a2c2090600b669ddac2727a7b880d9ee71742823
Matthew Reid <mreid@small-improvements.com>
2019-11-29 16:26:57 +0100

    Add footer

--- src/client/components/Footer/index.js
+++ src/client/components/Footer/index.js
@@ -1,32 +1,37 @@
 import React from 'react';
 import {Row, Col} from 'react-bootstrap';
 import styles from './style.less';
+import c from 'classnames';
 
 export default function Footer() {
   return (
-    <footer className={styles.footer}>
-      <Row>
-        <Col md={3}>
-          <h4 />
-          <small>© 2019 Codefish Apps Ltd, All rights reserved</small>
-        </Col>
-        <Col md={4}>
-          <h4 />
-          <Col md={6} />
-          <Col md={6} />
-        </Col>
-        <Col md={2}>
-          <h4>Follow Us</h4>
-          <ul>
-            <li>
-              <a href="https://www.twitter.com/helloflightpop">Twitter</a>
-            </li>
-          </ul>
-        </Col>
-        <Col md={3}>
-          Powered by <img src="/kiwi-logo.svg" width="100" />
-        </Col>
-      </Row>
+    <footer className={c(styles.footer, 'navbar-fixed-bottom')}>
+      <Col md={8} mdOffset={2}>
+        <Row>
+          <Col md={3}>
+            <h4 />
+            <small>
+              © 2019 Codefish Apps Ltd, <br /> All rights reserved
+            </small>
+          </Col>
+          <Col md={4}>
+            <h4 />
+            <Col md={6} />
+            <Col md={6} />
+          </Col>
+          <Col md={2}>
+            <h4>Follow Us</h4>
+            <ul className={styles.social}>
+              <li className={styles.twitter}>
+                <a href="https://www.twitter.com/helloflightpop">Twitter</a>
+              </li>
+            </ul>
+          </Col>
+          <Col md={3}>
+            Powered by <img src="/kiwi-logo.svg" width="100" />
+          </Col>
+        </Row>
+      </Col>
     </footer>
   );
 }
